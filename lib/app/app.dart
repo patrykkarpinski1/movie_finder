882a23817bf98633acb613b3f831357969de5dd7
Patryk Karpinski <patrykarpinski96@gmail.com>
2023-11-05 02:33:37 +0100

    Add Flavor to main

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_finder/app/core/config.dart';
 import 'package:movie_finder/auth/auth_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.black,
         scaffoldBackgroundColor: Colors.black,
       ),
+      debugShowCheckedModeBanner: Config.debugShowCheckedModeBanner,
       home: AuthPage(),
     );
   }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_finder/app/app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movie_finder/app/core/config.dart';
+import 'package:movie_finder/app/core/enums.dart';
 import 'package:movie_finder/app/injection_container.dart';
 import 'package:movie_finder/auth/cubit/auth_cubit.dart';
 
 Future main() async {
+  Config.appFlavor = Flavor.production;
   await dotenv.load(fileName: ".env");
   configureDependencies();
   runApp(
